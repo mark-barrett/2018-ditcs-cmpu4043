ffbafc0116607c3d0efc95740f0b22124185fb53
Mark Barrett <mark.barrett3@mydit.ie>
2018-12-02 17:27:38 +0000

    Updates

---
+++ precache-manifest.b446bdf5aa36246be0b22d4858fc58e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "e6d38ed55bfcb9b39dc8",
+    "url": "/2018-ditcs-cmpu4043/static/css/main.ced5211b.chunk.css"
+  },
+  {
+    "revision": "e6d38ed55bfcb9b39dc8",
+    "url": "/2018-ditcs-cmpu4043/static/js/main.e6d38ed5.chunk.js"
+  },
+  {
+    "revision": "34895df8048e06b5d2b7",
+    "url": "/2018-ditcs-cmpu4043/static/css/1.87d07c48.chunk.css"
+  },
+  {
+    "revision": "34895df8048e06b5d2b7",
+    "url": "/2018-ditcs-cmpu4043/static/js/1.34895df8.chunk.js"
+  },
+  {
+    "revision": "028b328a1b22ac98ed03",
+    "url": "/2018-ditcs-cmpu4043/static/js/runtime~main.028b328a.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/2018-ditcs-cmpu4043/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/2018-ditcs-cmpu4043/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/2018-ditcs-cmpu4043/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/2018-ditcs-cmpu4043/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/2018-ditcs-cmpu4043/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "949daaade37fbe87416d04954aaa4256",
+    "url": "/2018-ditcs-cmpu4043/index.html"
+  }
+];
